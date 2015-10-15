8e2458bf1eace730e889cd3e340cc81f2ce56e01
Jessica May <mayjessica13@gmail.com>
2015-10-14 21:31:51 -0400

    main contact scripts updated

---
+++ scripts.js
@@ -0,0 +1,8 @@
+	function myFunction() {
+    var mail = prompt("Please enter your email address.");
+    
+    if (mail != null) {
+        document.getElementById("email").innerHTML =
+        "Thank you for joining the mailing list.";
+    	}
+	}
