f9a66a8519c7b179a31eb75d0f3599ea0a05a6fe
Jessica May <mayjessica13@gmail.com>
2015-10-15 18:29:11 -0400

    final contact

--- scripts.js
+++ scripts.js
@@ -24,12 +24,3 @@
     $("#work").mouseleave(function() {
 	  	$("#work").fadeTo("fast", 1);
 	  })
-    /*Creates fake mailing list button*/
-    function myFunction() {
-    var mail = prompt("Please enter your email address.");
-    
-    if (mail != null) {
-        document.getElementById("email").innerHTML =
-        "Thank you for joining the mailing list.";
-        }
-    }
