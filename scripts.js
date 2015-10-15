927a7554f83e8950c563f868513d27ea525590cd
Saloni Sharma <saloni.sharma@gmail.com>
2015-10-15 17:49:40 -0400

    save this

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
+$(document).ready(function(){
+
+	//Creates a sliding effect to emphasize div 'facts'
+	$("#facts").slideUp(600);
+	$("#facts").slideDown(600);
+
+	//Reduces opacity when mouse enters divs 'mission' 'vision' and 'work' - then returns opacity to normal upon exit
+  	$("#mission").mouseenter(function() {
+        $("#mission").fadeTo("fast", 0.75);
+    })
+    $("#mission").mouseleave(function() {
+	  	$("#mission").fadeTo("fast", 1);
+	  })
+	$("#vision").mouseenter(function() {
+    	$("#vision").fadeTo("fast", 0.75);
+    })
+    $("#vision").mouseleave(function() {
+	  	$("#vision").fadeTo("fast", 1);
+	  })
+
+    $("#work").mouseenter(function() {
+        $("#work").fadeTo("fast", 0.75);
+    })
+    $("#work").mouseleave(function() {
+	  	$("#work").fadeTo("fast", 1);
+	  })
 	function myFunction() {
     var mail = prompt("Please enter your email address.");
     
@@ -6,3 +32,4 @@
         "Thank you for joining the mailing list.";
     	}
 	}
+	})
